4715540ea1213f8e82ea2f3ea78d97c016154df1
Taras Shkodenko <taras@shkodenko.com>
2020-10-07 16:30:02 +0300

    Started to implement login/login action

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -8,10 +8,31 @@
  * @author Taras Shkodenko <taras@shkodenko.com>
  */
 
+use Phalcon\Http\Request;
+
+
 class LoginController extends ControllerBase
 {
     public function indexAction()
     {
 
+    }
+
+    public function loginAction()
+    {
+        $request = new Request();
+
+        if ($request->isPost()) {
+            $login = $request->getPost('login');
+            error_log(__METHOD__ . ' +' . __LINE__ . ' login: ' . var_export($login, true));
+            $password = $request->getPost('password');
+            error_log(__METHOD__ . ' +' . __LINE__ . ' password: ' . var_export($password, true));
+
+            $user = User::find(['conditions' => 'login = :login: AND password = :password:',
+                'bind' => ['login' => $login, 'password' => $password]
+            ]);
+
+            error_log(__METHOD__ . ' +' . __LINE__ . ' found user: ' . print_r($user, true));
+        }
     }
 }
