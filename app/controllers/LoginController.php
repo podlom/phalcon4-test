0190f5fe1504b449c8d9499d675066096bdfe611
Taras Shkodenko <taras@shkodenko.com>
2020-10-07 16:23:27 +0300

    Started login controller

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Taras
+ * Date: 07.10.2020
+ * Time: 16:20
+ *
+ * @author Taras Shkodenko <taras@shkodenko.com>
+ */
+
+class LoginController extends ControllerBase
+{
+    public function indexAction()
+    {
+
+    }
+}
